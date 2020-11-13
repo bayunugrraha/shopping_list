3c334a05f1407e3f7918df4df86dba269309a49e
Bayu <bayungraha@gmail.com>
2020-11-13 17:45:49 +0700

    add new data item

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './components/product_lists.dart';
 import './components/dashboard.dart';
+import './components/add_new_item.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,6 +31,30 @@ class _HomeState extends State<Home> {
       Cart(id: 'DW1', title: 'Sabun Mandi', harga: 15000, qty: 1),
       Cart(id: 'DW2', title: 'Shampoo', harga: 17000, qty: 2),
     ];
+
+    void _openModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return AddNewItem(_addNewItem); //DAN ISI DARI MODAL TERSEBUT ADALAH COMPONENT AddNewItem. PASTINYA PARAMETER YANG DIKIRIM ADALAH SEBUAH FUNGSI BERNAMA _addNewItem, MAKA PERLU KITA DEFINISIKAN SELANJUTNYA
+        });
+  }
+
+  //FUNGSI INI UNTUK MEMANIPULASI DENGAN MENAMBAHKAN DATA BARU KE DALAM CART
+  void _addNewItem(String title, double harga, int qty) {
+    //BUAT FORMAT DATANYA DENGAN REFERENSI MENGGUNAKAN MODAL Cart
+    final newItem = Cart(id: DateTime.now().toString(), title: title, harga: harga, qty: qty);
+    setState(() {
+      _carts.add(newItem); //SET STATE-NYA UNTUK MENAMBAHKAN DATA BARU TERSEBUT
+    });
+  }
+  
+  //FUNGSI INI UNTUK MENGHAPUS SEMUA DATA PADA VARIABLE CARTS
+  void _resetCarts() {
+    setState(() {
+      _carts.clear(); //SET STATENYA KEMUDIAN CLEAR
+    });
+  }
   }
 
   @override
